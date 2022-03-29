41765a3825e75b8095e628d8d521bd6cdd19237e
lavpatil <36926481+lavpatil@users.noreply.github.com>
2022-03-29 17:29:22 +0530

    Update arg.py

--- arg.py
+++ arg.py
@@ -1,3 +1,7 @@
 import os
 
-print(os.environ[''])
+print(os.environ['Build JobName'])
+print(os.environ['Build No.'])
+print(os.environ['Build JobTS'])
+print(os.environ['Build JobDuration'])
+print(os.environ['Build Date'])
