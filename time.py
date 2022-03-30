572084562e4e61ed9801e3f68270e7e2b587a80e
lavpatil <36926481+lavpatil@users.noreply.github.com>
2022-03-30 18:51:17 +0530

    Update time.py

--- time.py
+++ time.py
@@ -1,4 +1,37 @@
-http://localhost:8080/job/jobA/100/buildNumber
-http://localhost:8080/job/jobA/100/buildTimestamp
-http://jenkins:8080/job/JOB_NAME/lastBuild/buildNumber
-http://jenkins:8080/job/JOB_NAME/lastBuild/api/json?tree=builds%5Bnumber,status,timestamp,id,result%5D
+from urllib.request import urlopen
+    from urllib.parse import quote
+except ImportError:  # Python 2
+    from urllib2 import urlopen, quote
+
+import json
+import contextlib
+
+status_list_dict = {}
+with contextlib.closing(
+    urlopen("http://localhost:8080/api/json")
+) as job_list_response:
+    job_list = json.load(job_list_response)["jobs"]
+
+for job in job_list:
+    status_counts = [0,0,0,0]
+
+    with contextlib.closing(
+        urlopen(
+            "http://localhost:8080/job/{job_name}/api/json?tree=allBuilds[result]".format(
+                job_name=quote(job["name"])
+            )
+        )
+    ) as build_list_response:
+        build_list = json.load(build_list_response)["allBuilds"]
+
+        for build_data in build_list:
+            if build_data["result"] == "SUCCESS":
+                status_counts[0] += 1
+            elif build_data["result"] == "FAILURE":
+                status_counts[1] += 1
+            elif build_data["result"] == "UNSTABLE":
+                status_counts[2] += 1
+            elif build_data["result"] == "ABORTED":
+                status_counts[3] += 1
+
+     status_list_dict[job["name"]] = status_counts
