6c56c494e8082f0dcbb2ba1a49edab7af5133751
lavpatil <36926481+lavpatil@users.noreply.github.com>
2022-03-30 17:38:36 +0530

    Create time.py

---
+++ time.py
@@ -0,0 +1,10 @@
+def calculateDuration(start, end) {
+    long elapsedTime = end - start
+
+    Long second = (elapsedTime / 1000).longValue() % 60;
+    Long minute = (elapsedTime / (1000 * 60)).longValue() % 60;
+    Long hour = (elapsedTime / (1000 * 60 * 60)).longValue() % 24;
+    Long remainderMillis = elapsedTime % 1000
+
+    return "${hour}h ${minute}m ${second}s ${remainderMillis}ms"
+}
