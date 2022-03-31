2df135b9fee9eba8fb8436814c90e43e34953911
lavpatil <36926481+lavpatil@users.noreply.github.com>
2022-03-31 11:35:21 +0530

    Update build.py

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 import hudson.plugins.timestamper.api.TimestamperAPI;
 import java.io.BufferedReader;
 
-String query = "time=HH:mm:ss";
+String = "time=HH:mm:ss";
 try (BufferedReader reader = TimestamperAPI.get().read(build, query)) {
     // read timestamps here
 }
