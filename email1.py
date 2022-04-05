1be0a57b422381274c427b29127957c44927f16e
lavpatil <36926481+lavpatil@users.noreply.github.com>
2022-04-05 11:46:03 +0530

    Update email1.py

--- email1.py
+++ email1.py
@@ -1,34 +1,18 @@
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 import smtplib
-  
 
-msg = MIMEMultipart()
-  
-  
-message = "Thank you"
-  
+sender = 'lavpatil2015@gmail.com'
+receivers = ['lavpatil2015@gmail.com']
 
-password = "nokia@2021"
-msg['From'] = "lavpatil2015@gmail.com"
-msg['To'] = "lavpatil2015@gmail.com"
-msg['Subject'] = "Subscription"
-  
+message = """From: From Person <lavpatil2015@gmail.com>
+To: To Person <lavpatil2015@gmail.com>
+Subject: SMTP e-mail test
 
-msg.attach(MIMEText(message, 'plain'))
-  
+This is a test e-mail message.
+"""
 
-server = smtplib.SMTP('smtp.gmail.com: 587')
-  
-server.starttls()
-  
-
-server.login(msg['From'], password)
-  
-  
-
-server.sendmail(msg['From'], msg['To'], msg.as_string())
-  
-server.quit()
-  
-print("successfully sent email to %s:" % (msg['To']))
+try:
+   smtpObj = smtplib.SMTP('localhost')
+   smtpObj.sendmail(sender, receivers, message)         
+   print "Successfully sent email"
+except SMTPException:
+   print "Error: unable to send email"
