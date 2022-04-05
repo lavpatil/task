9af2fe7cca4cccd50893f7ed488400a686d87d94
lavpatil <36926481+lavpatil@users.noreply.github.com>
2022-04-05 11:35:56 +0530

    Update email1.py

--- email1.py
+++ email1.py
@@ -1,34 +1,32 @@
-# import necessary packages
-  
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
   
-# create message object instance
+
 msg = MIMEMultipart()
   
   
 message = "Thank you"
   
-# setup the parameters of the message
+
 password = "nokia@2021"
 msg['From'] = "lavpatil2015@gmail.com"
 msg['To'] = "lavpatil2015@gmail.com"
 msg['Subject'] = "Subscription"
   
-# add in the message body
+
 msg.attach(MIMEText(message, 'plain'))
   
-#create server
+
 server = smtplib.SMTP('smtp.gmail.com: 587')
   
 server.starttls()
   
-# Login Credentials for sending the mail
+
 server.login(msg['From'], password)
   
   
-# send the message via the server.
+
 server.sendmail(msg['From'], msg['To'], msg.as_string())
   
 server.quit()
