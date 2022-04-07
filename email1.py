ee7f9242e842d412178bd58c6774ed9b7b4c66e1
lavpatil <36926481+lavpatil@users.noreply.github.com>
2022-04-07 13:20:24 +0530

    Update email1.py

--- email1.py
+++ email1.py
@@ -1,51 +1,75 @@
-#!/usr/bin/python
 
-from cgitb import html
+import smtplib, ssl
+
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import smtplib
 
-gmail_user = 'lavpatil2015@gmail.com'
-gmail_password = 'nokia@2021'
+import pandas as pd
 
-sender = 'lavpatil2015@gmail.com'
-receivers = ['lavpatil2015@gmail.com']
 
-html = """
-<!DOCTYPE html>
-<html>
-   <head>
-      <style>
-         table, th, td {
-            border: 1px solid black;
-         }
-      </style>
-   </head>
-   <body>
-      <h2>JOB Details</h2>
-      <table>
-         <tr>
-            <th>Job Name</th>
-            <th>Build Number</td>
-            <th>Job URL</td>
-         </tr>
-         <tr>
-            <td>${JOB_NAME}</td>
-            <td>${BUILD_NUMBER}</td>
-            <td>${JOB_URL}</td>
-         </tr>
-      </table>
-   </body>
+html = '''
+html>
+<p style="color : black ; font-size : 11px;">Hi All,</p>
+<head>
+<style>
+table {
+font-family: arial, sans-serif;
+border-collapse: collapse;
+width: 100%;
+}
+td, th, td {
+      
+      text-align: left;
+      padding: 8px;
+}
+tr:nth-child(even) {
+background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+<h2>Jobs Performed</h2>
+<table>
+<tr>
+<th>Job Name</th>
+<th>Build Number</th>
+<th>Build URL</th>
+</tr>
+<tr>
+<td>${JOB_NAME}</td>
+<td>${BUILD_NUMBER}</td>
+<td>${BUILD_URL}</td>
+</tr>
+</tr>
+</table>
+<p>Note:-</p>
+<p style="color : black ; font-size : 11px;">*This is auto generated mail kindly do not reply.</p>
+<p style="color : black ; font-size : 11px;">*For any build query contact CI-Team@kpit.com.</p>
+</body>
 </html>
-"""
+    '''
 
-try:
-   email_message = MIMEMultipart()
-   email_message.attach(MIMEText(html, "html"))
-   email_string = email_message.as_string()
-   smtpObj = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-   smtpObj.login(gmail_user, gmail_password)
-   smtpObj.sendmail(sender, receivers, email_string)         
-   print ("Successfully sent email")
-except Exception as ex:
-   print ("Error: unable to send email",ex)
+
+email_from = 'lavpatil2015@gmail.com'
+password = 'nokia@2021'
+email_to = 'lavpatil2015@gmail.com'
+
+
+date_str = pd.Timestamp.today().strftime('%Y-%m-%d')
+
+
+email_message = MIMEMultipart()
+email_message['From'] = email_from
+email_message['To'] = email_to
+email_message['Subject'] = f'Report email - {date_str}'
+
+
+email_message.attach(MIMEText(html, "html"))
+
+email_string = email_message.as_string()
+
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+   server.login(email_from, password)
+   server.sendmail(email_from, email_to, email_string)
