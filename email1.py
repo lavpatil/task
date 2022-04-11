81bd7723d3d36b002bcf072a42c2e95d48d19a33
lavpatil <36926481+lavpatil@users.noreply.github.com>
2022-04-11 15:30:09 +0530

    Update email1.py

--- email1.py
+++ email1.py
@@ -10,7 +10,7 @@ gmail_password = 'nokia@2021'
 sender = 'lavpatil2015@gmail.com'
 receivers = ['lavpatil2015@gmail.com']
 
-txt = "{jobname:Job_NAME}"
+txt = {jobname:'Job_NAME'}
 
 message = """From: From Person <lavpatil2015@gmail.com>
 To: To Person <lavpatil2015@gmail.com>
