6ec5521e5880cd94ce4569aa4337c7d275549014
lavpatil <36926481+lavpatil@users.noreply.github.com>
2022-04-21 12:50:21 +0530

    Update kafk.py

--- kafk.py
+++ kafk.py
@@ -5,4 +5,10 @@ from requests.auth import HTTPBasicAuth
 res = requests.get('http://localhost:9090/, ',auth = HTTPBasicAuth('lav', 'Nokia@2021'))
 
 
-print(res)
+info = server.get_job_info('job-name')
+# Loop over builds
+builds = info['builds']
+for build in builds:
+    for build in builds:
+        print(server.get_build_info('job-name', 
+                                    build['number']))
