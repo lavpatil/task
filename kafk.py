9585742abcd9e5396258848fd7729a2f0061588a
lavpatil <36926481+lavpatil@users.noreply.github.com>
2022-04-21 12:45:03 +0530

    Update kafk.py

--- kafk.py
+++ kafk.py
@@ -1,9 +1,8 @@
-# import requests module
 import requests
 from requests.auth import HTTPBasicAuth
 
-# Making a get request
+
 res = requests.get('http://localhost:9090/, ',auth = HTTPBasicAuth('lav', 'Nokia@2021'))
 
-# print request object
+
 print(res)
