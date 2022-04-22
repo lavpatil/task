e2968498115034b4c95a51edae3966990bb79f90
lavpatil <36926481+lavpatil@users.noreply.github.com>
2022-04-22 17:28:11 +0530

    Update kafk.py

--- kafk.py
+++ kafk.py
@@ -1,8 +1,33 @@
+import json
+import datetime
+from datetime import datetime
+from decimal import Decimal
 import requests
 from requests.auth import HTTPBasicAuth
 
 
 res = requests.get("http://localhost:9090/job/jenk1/6/wfapi",auth = HTTPBasicAuth('lav', 'Nokia@2021'))
 
+#print(type(res.text))
+j_object=json.loads(res.text)
 
-print(res.text)
+with open('dat.json', 'w') as f:
+    json.dump(j_object, f)
+
+f=open('dat.json')
+data=json.load(f)
+
+def d_json():
+    job_name=data['name']
+    job_id=data['id']
+    job_status=data['status']
+    starttime=datetime.fromtimestamp(data['startTimeMillis']//1000).isoformat()
+    endtime=datetime.fromtimestamp(data['endTimeMillis']//1000).isoformat()
+
+    job_details = {"job_name":job_name,"job_id":job_id,"job_status":job_status,"starttime":starttime,"endtime":endtime}
+    return(job_details)
+
+jo=d_json()
+print(jo)
+
+
